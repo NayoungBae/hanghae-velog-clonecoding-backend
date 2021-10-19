8ba23e405781142499360b22dea038e47392edf3
slsnrnsep <gkrwjaeoqkr1@gmail.com>
2021-10-19 16:26:04 +0900

    add : comment function2

--- src/main/java/com/hanghae/velog/dto/CommentDto.java
+++ src/main/java/com/hanghae/velog/dto/CommentDto.java
@@ -1,4 +1,13 @@
 package com.hanghae.velog.dto;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CommentDto {
+    private String content;
+    private String username;
+    private Long postId;
 }
--- src/main/java/com/hanghae/velog/model/Comment.java
+++ src/main/java/com/hanghae/velog/model/Comment.java
@@ -1,4 +1,31 @@
 package com.hanghae.velog.model;
 
-public class Comment {
+import com.hanghae.velog.dto.CommentDto;
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Comment extends Timestamped{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private String comments;
+
 }
